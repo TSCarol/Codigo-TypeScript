358abab4b266550124f11d25ae420b504b6f90a6
TSCarol <thaiscsantoos@gmail.com>
2024-04-02 18:48:05 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,10 @@
+//multiplicação
+function exercicio1(primeiroNumero, segundoNumero) {
+    return primeiroNumero * segundoNumero;
+}
+//saudação
+function exercicio2(nome) {
+    return " Olá " + nome;
+}
+console.log(exercicio1(5, 3));
+console.log(exercicio2("Arya"));
---
+++ main.ts
@@ -0,0 +1,13 @@
+//multiplicação
+function exercicio1(primeiroNumero: number, segundoNumero: number): number {
+    return primeiroNumero * segundoNumero;
+}
+
+//saudação
+function exercicio2(nome: string): string {
+    return " Olá " + nome;
+}
+
+
+console.log(exercicio1(5, 3))
+console.log(exercicio2("Arya"))
